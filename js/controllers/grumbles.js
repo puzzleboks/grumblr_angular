55143cb754fc06d0e62e95df3af171d7172e1554
Jesse Shawl <jesse@jshawl.com>
2015-09-06 13:03:15 -0400

    create comments working

--- js/controllers/grumbles.js
+++ js/controllers/grumbles.js
@@ -7,12 +7,21 @@
   }]);
 
   // show controller (handles delete link on show page)
-  grumbleControllers.controller('grumbleController', ['$routeParams','$location','$http', 'Grumble', function($routeParams, $location, $http, Grumble){
-    this.grumble = Grumble.get({id: $routeParams.id});
+  grumbleControllers.controller('grumbleController', ['$routeParams','$location','$http', 'Grumble','Comment', function($routeParams, $location, $http, Grumble, Comment){
+    this.grumble = Grumble.get({id: $routeParams.id}, function(grumble){
+      grumble.comments = Comment.query({grumbleId: $routeParams.id});
+    });
     this.delete = function(id){
       Grumble.delete({id: id}, function(){
 	$location.path("/grumbles")
       });
+    }
+    this.createComment = function(comment){
+      var self = this
+      Comment.save({grumbleId: $routeParams.id},comment, function(comment){
+	self.grumble.comments.push(comment)
+	self.comment = {}
+      })
     }
   }]);
 
--- js/services/grumble.js
+++ js/services/grumble.js
@@ -1,8 +1,12 @@
 (function() {
   var grumbleServices = angular.module('grumbleServices', ['ngResource']);
-
   grumbleServices.factory('Grumble', ['$resource', function($resource) {
     return $resource('http://grumblr.wdidc.org/grumbles/:id', {}, {
+      update: {method:'PUT'}
+    });
+  }]);
+  grumbleServices.factory('Comment', ['$resource', function($resource) {
+    return $resource('http://grumblr.wdidc.org/grumbles/:grumbleId/comments/:id', {}, {
       update: {method:'PUT'}
     });
   }]);
