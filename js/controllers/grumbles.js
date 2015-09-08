b748f1d89b8d9ad95d6c2b7736ecc65d8d3b1c34
Adam Bray <adam.bray@yale.edu>
2015-09-08 13:48:03 -0400

    refactor to move controllers to named functions

--- js/controllers/grumbles.js
+++ js/controllers/grumbles.js
@@ -1,37 +1,10 @@
 (function() {
   app = angular.module('grumbleControllers',[]);
 
-  app.controller('grumbleController', ['Grumble', '$routeParams', '$location', function(Grumble, $routeParams, $location) {
-    this.grumble = Grumble.get({id: $routeParams.id});
-    this.delete = function() {
-      Grumble.delete({id: $routeParams.id}, function() {
-        $location.path('/grumbles');
-      });
-    }
-  }])
-
-  app.controller('newGrumbleController', ['Grumble', '$location', function(Grumble, $location) {
-    this.create = function() {
-      Grumble.save(this.grumble, function() {
-        $location.path('/grumbles');
-      });
-    }
-  }])
-
-  app.controller('editGrumbleController', ['Grumble', '$routeParams', '$location', function(Grumble, $routeParams, $location) {
-    this.grumble = Grumble.get({id: $routeParams.id});
-    this.update = function() {
-      var self = this;
-      this.grumble.$update({id: this.grumble.id}, function() {
-        $location.path('/grumbles/' + self.grumble.id);
-      });
-    }
-  }])
-
-
-  app.controller('grumblesController', ['Grumble', function(Grumble) {
-    this.grumbles = Grumble.query();
-  }]);
+  app.controller('grumbleController', ['Grumble', '$routeParams', '$location', showGrumble]);
+  app.controller('newGrumbleController', ['Grumble', '$location', newGrumble]);
+  app.controller('editGrumbleController', ['Grumble', '$routeParams', '$location', editGrumble ]);
+  app.controller('grumblesController', ['Grumble', indexGrumbles]);
 
   app.controller( 'commentsController', function(){
     this.create = function( grumble ){
@@ -39,6 +12,38 @@
         content: grumble.newCommentContent
       });
       grumble.newCommentContent = "";
-    }
-  })
+    };
+  });
 })();
+
+
+function showGrumble(Grumble, $routeParams, $location) {
+  this.grumble = Grumble.get({id: $routeParams.id});
+  this.delete = function() {
+    Grumble.delete({id: $routeParams.id}, function() {
+      $location.path('/grumbles');
+    });
+  };
+}
+
+function newGrumble(Grumble, $location) {
+  this.create = function() {
+    Grumble.save(this.grumble, function() {
+      $location.path('/grumbles');
+    });
+  };
+}
+
+function editGrumble(Grumble, $routeParams, $location) {
+  this.grumble = Grumble.get({id: $routeParams.id});
+  this.update = function() {
+    var self = this;
+    this.grumble.$update({id: this.grumble.id}, function() {
+      $location.path('/grumbles/' + self.grumble.id);
+    });
+  };
+}
+
+function indexGrumbles(Grumble) {
+  this.grumbles = Grumble.query();
+}
