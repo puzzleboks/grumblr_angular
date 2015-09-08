02880553a8048887c0baee686f255fa184d68981
Adam Bray <adam.bray@yale.edu>
2015-09-08 14:48:46 -0400

    added comment create and delete

--- js/controllers/grumbles.js
+++ js/controllers/grumbles.js
@@ -5,15 +5,6 @@
   app.controller('newGrumbleController', ['Grumble', '$location', newGrumble]);
   app.controller('editGrumbleController', ['Grumble', '$routeParams', '$location', editGrumble ]);
   app.controller('grumblesController', ['Grumble', indexGrumbles]);
-
-  app.controller( 'commentsController', function(){
-    this.create = function( grumble ){
-      grumble.comments.unshift({
-        content: grumble.newCommentContent
-      });
-      grumble.newCommentContent = "";
-    };
-  });
 })();
 
 
@@ -21,11 +12,29 @@ function showGrumble(Grumble, Comment, $routeParams, $location) {
   this.grumble = Grumble.get({id: $routeParams.id}, function(grumble) {
     grumble.comments = Comment.query({grumble_id: grumble.id});
   });
+
   this.delete = function() {
     Grumble.delete({id: $routeParams.id}, function() {
       $location.path('/grumbles');
     });
   };
+
+  this.createComment = function() {
+    var self = this;
+    Comment.save({grumble_id: this.grumble.id}, this.newComment, function(comment) {
+      // self.grumble.comments = Comment.query({grumble_id: self.grumble.id});
+      self.grumble.comments.push(comment);
+      self.newComment = {};
+    });
+  };
+
+  this.deleteComment = function(comment) {
+    var self = this;
+    Comment.delete({grumble_id: this.grumble.id, id: comment.id}, function() {
+      self.grumble.comments = Comment.query({grumble_id: self.grumble.id});
+    });
+  };
+
 }
 
 function newGrumble(Grumble, $location) {
