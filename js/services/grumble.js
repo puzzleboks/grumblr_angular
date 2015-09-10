f1c0431a67a342372f3d876415b8461ff2914cf8
lon phelps <puzzleboks@gmail.com>
2015-09-09 22:48:31 -0400

    Tried removing json from resource

--- js/services/grumble.js
+++ js/services/grumble.js
@@ -1,6 +1,8 @@
 (function() {
   var grumbleServices = angular.module('grumbleServices', []);
   grumbleServices.factory('Grumble', ['$resource', function($resource) {
-    return $resource('http://grumblr.wdidc.org/grumbles/:id.json');
+    return $resource('http://grumblr.wdidc.org/grumbles/:id', {}, {
+      update: {method:'PUT'}
+    });
   }]);
 })();
