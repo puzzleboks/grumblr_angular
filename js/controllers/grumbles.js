c30c807514ffd9513114c2a5db576a196bec7ee6
lon phelps <puzzleboks@gmail.com>
2015-09-10 13:33:44 -0400

    in class firebase assignment

--- js/controllers/grumbles.js
+++ js/controllers/grumbles.js
@@ -8,19 +8,19 @@
 
   // show controller (handles delete link on show page)
   grumbleControllers.controller('grumbleController', ['$routeParams','$location','$http', 'Grumble','Comment', function($routeParams, $location, $http, Grumble, Comment){
-    this.grumble = Grumble.get({id: $routeParams.id}, function(grumble){
+    this.grumble = Grumble.get({$id: $routeParams.id}, function(grumble){
       grumble.comments = Comment.query({grumbleId: $routeParams.id});
     });
     this.delete = function(id){
-      Grumble.delete({id: id}, function(){
-	$location.path("/grumbles")
+      Grumble.delete({$id: id}, function(){
+        $location.path("/grumbles")
       });
     }
     this.createComment = function(comment){
       var self = this
       Comment.save({grumbleId: $routeParams.id},comment, function(comment){
-	self.grumble.comments.push(comment)
-	self.comment = {}
+        self.grumble.comments.push(comment)
+        self.comment = {}
       })
     }
   }]);
--- js/services/grumble.js
+++ js/services/grumble.js
@@ -1,10 +1,50 @@
 (function() {
-  var grumbleServices = angular.module('grumbleServices', ['ngResource']);
-  grumbleServices.factory('Grumble', ['$resource', function($resource) {
-    return $resource('http://grumblr.wdidc.org/grumbles/:id', {}, {
-      update: {method:'PUT'}
-    });
-  }]);
+  var firebaseUrl = "https://wdi-grumbler-lwp.firebaseio.com";
+  var connectedRef = new Firebase(firebaseUrl + "/.info/connected");
+  connectedRef.on("value", function(snapshot) {
+    if (snapshot.val() === true) {
+      console.log("Connected to firebase:", firebaseUrl);
+    } else {
+      console.log("Not connected to firebase:", firebaseUrl);
+    }
+  });
+  var grumbleServices = angular.module('grumbleServices', ['ngResource', 'firebase']);
+
+  grumbleServices.factory('Grumble', ['$firebaseObject','$firebaseArray', grumbleFirebase]);
+  // Manages resources in firebase
+  // mirrors ngResource interface
+  function grumbleFirebase($firebaseObject, $firebaseArray) {
+    // get reference to grumbles "namespace"
+    var grumblesRef = new Firebase(firebaseUrl + "/grumbles");
+    // retrieve our grumbles
+    var grumbles = $firebaseArray(grumblesRef);
+    // mirror ngResource interface
+    var Grumble = {
+      query: function(){ return grumbles; },
+      save: function( grumble, callback ){
+        // persist our grumble
+        grumbles.$add(grumble).then(function(ref) {
+          grumble.$id = ref.key();
+          // and pass the saved grumble back
+          callback(grumble);
+        });
+      },
+      get: function (grumble, callback) {
+        // var found = $firebaseObject(grumblesRef.child(grumble.$id));
+        var found = $firebaseObject(grumblesRef.child(grumble.$id));
+        // support optional callback
+        if(typeof callback == "function") {
+          callback(found);
+        }
+        return found;
+      },
+      delete: function(grumble, cb){
+        return grumbles.$remove(grumbles.$getRecord(grumble.$id));
+      }
+    };
+    return Grumble;
+  };
+
   grumbleServices.factory('Comment', ['$resource', function($resource) {
     return $resource('http://grumblr.wdidc.org/grumbles/:grumbleId/comments/:id', {}, {
       update: {method:'PUT'}
